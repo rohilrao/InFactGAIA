a0cfa2e7225342956f4c7702b622b1466610b856
rohilrao <rohil93@gmail.com>
2025-03-04 14:58:03 +0100

    use default navigation

--- src/app.py
+++ src/app.py
@@ -2,16 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="InFactGAIA", page_icon="📂", layout="wide")
 
-st.sidebar.title("🔍 Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Hypothesis Explorer"])
-
-if page == "Home":
-    st.title("🏠 Welcome to InFactGAIA")
-    st.write("Use the sidebar to navigate through the app.")
-
-elif page == "Hypothesis Explorer":
-    from pages.hypothesis_explorer import hypothesis_explorer
-    hypothesis_explorer()
-
-
-
+st.title("🏠 Welcome to InFactGAIA")
+st.write("Use the sidebar to navigate through the app.")
