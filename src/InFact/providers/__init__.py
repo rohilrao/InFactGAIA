ef41dc7439a8b8885f306e7d9f178d4e8e9c4493
rohilrao <rohil93@gmail.com>
2025-04-10 13:34:55 +0200

    delete unused files

--- src/InFact/providers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LLM Provider implementations for InFact.
-"""
-
-from .llm_provider import LLMProvider
-from .anthropic_provider import AnthropicProvider
-from .openai_provider import OpenAIProvider
-
-__all__ = ['LLMProvider', 'AnthropicProvider', 'OpenAIProvider']
